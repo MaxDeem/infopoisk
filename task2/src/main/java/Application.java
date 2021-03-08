d53dc9c5622e4f2012feb1d4d9b1f1d83e4e470a
Maxim <maksim.g14@mail.ru>
2021-03-09 01:31:53 +0300

    minimal processing done

--- task2/src/main/java/Application.java
+++ task2/src/main/java/Application.java
@@ -1,24 +1,36 @@
 import lombok.SneakyThrows;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Application {
+    public static String outputToken = "E:\\studies\\infopoisk\\infopoisk\\task2\\src\\main\\resources\\tokens.txt";
     @SneakyThrows
-    public static void main(String[] args) {
-        String file = "E:\\studies\\infopoisk\\infopoisk\\task2\\src\\main\\resources\\test.txt";
-        BufferedReader reader = new BufferedReader(new FileReader(file));
+    public static void getAllTokens(BufferedReader reader, BufferedWriter writer) {
         String line = reader.readLine();
         while (line != null) {
             StringTokenizer tokenizer = new StringTokenizer(line);
             while (tokenizer.hasMoreTokens()) {
-                System.out.println(tokenizer.nextToken());
+                addToken(writer, tokenizer.nextToken());
             }
             line = reader.readLine();
         }
         reader.close();
+    }
 
+    @SneakyThrows
+    public static void addToken(BufferedWriter writer, String token) {
+        writer.write(token);
+        writer.newLine();
+        writer.flush();
+    }
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        String file = "E:\\studies\\infopoisk\\infopoisk\\task2\\src\\main\\resources\\test.txt";
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outputToken));
+
+        getAllTokens(reader, writer);
     }
 }
