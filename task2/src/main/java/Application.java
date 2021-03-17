a17611db51511cc255865498aa892491b340c7c6
Maxim <maksim.g14@mail.ru>
2021-03-17 03:43:30 +0300

    configured pom.xml

--- task2/src/main/java/Application.java
+++ task2/src/main/java/Application.java
@@ -3,6 +3,6 @@ public class Application {
     public static String input = "src/main/resources/GMA The Heart of Asia - Wikipedia.html";
     public static String tokenFile = "src/main/resources/tokens.txt";
     public static void main(String[] args) {
-        
+
     }
 }
